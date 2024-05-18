65b876463db9e217958d40d92d88a0be4356e0bf
MPJBardini <jhony_bardini@hotmail.com>
2024-05-18 14:09:53 -0300

    Sistema Banco

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    double saldo = 25;
+    double valorSolicitado = 18;
+    System.out.println("Seu saldo atual é: R$" + saldo);
+    System.out.println("Digite o valor que deseja sacar: " + valorSolicitado);
+    
+    
+    if (saldo >= valorSolicitado) {
+      saldo -= valorSolicitado;
+      System.out.println("Saque realizado com sucesso!");
+      System.out.println("Seu novo saldo é: R$" + saldo);
+    } else {
+      System.out.println("Saldo insuficiente para realizar o saque de: R$" + valorSolicitado);
     }
+
+    scanner.close();
+
+    saldo = 15;
+    valorSolicitado = 22;
+    System.out.println("Seu saldo atual é: R$" + saldo);
+    System.out.println("Digite o valor que deseja sacar: " + valorSolicitado);
+    
+    if (saldo >= valorSolicitado) {
+      saldo -= valorSolicitado;
+      System.out.println("Saque realizado com sucesso!");
+      System.out.println("Seu novo saldo é: R$" + saldo);
+    } else {
+      System.out.println("Saldo insuficiente para realizar o saque de R$" + valorSolicitado);
+    }
+
+    scanner.close();
+  }
 }
